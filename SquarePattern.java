bf2f1d61b57dce513e32bd80b160d55fc45bb9b3
Vickycoder123 <vikaskumarg800@gmail.com>
2023-02-03 19:09:49 +0530

    Square Star pattern

--- SquarePattern.java
+++ SquarePattern.java
@@ -1,5 +1,15 @@
 import java.util.Scanner;
 
 public class SquarePattern {
-    
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int noOfLines = sc.nextInt();
+
+        for(int i = 0; i < noOfLines; i++){
+            for( int j = 0; j < noOfLines; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
 }
