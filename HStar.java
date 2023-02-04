451a66a7e50006a5ee884fa24f4876e94afd8646
Vickycoder123 <vikaskumarg800@gmail.com>
2023-02-04 13:21:35 +0530

    H Start pattern added

---
+++ HStar.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class HStar{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for( int i = 0; i < n; i++){
+            for( int j = 0; j < n; j++){
+                if( j == 0 || j == n-1 || i == (n-1)/2){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
