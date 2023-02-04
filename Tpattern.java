243b8d04963d25a9e5e6043391493011aadd715d
Vickycoder123 <vikaskumarg800@gmail.com>
2023-02-04 14:31:56 +0530

    TPattern Updated

--- Tpattern.java
+++ Tpattern.java
@@ -1,16 +1,15 @@
-import java.util.Scanner;
-
+import java.util.*;
 public class Tpattern {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int noOfLines = scan.nextInt();
-
-        for( int i = 0; i < noOfLines; i++){
-            for( int j = 0; j < noOfLines; j++){
-                if( i == 0 || j == 0 || i == (noOfLines-1)/2 || j == (noOfLines - 1)){
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for( int i = 0; i < n; i++){
+            for( int j = 0; j < n; j++){
+                if( i == 0 || j == (n-1)/2){
                     System.out.print("*");
-                }else{
-                    System.out.println(" ");
+                }
+                else{
+                    System.out.print(" ");
                 }
             }
             System.out.println();
